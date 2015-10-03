cbeae3a1a55b8ee9e0a1ed10b6b16353885ef4ad
Hongyuan Wang <710136012@qq.com>
2015-10-03 15:13:25 +0800

    use Spring MVC to actor as a MVC framework

---
+++ src/main/java/com/yuanwhy/fantasy/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.yuanwhy.fantasy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		model.addAttribute("message", "Hello");
+		return "home-index";
+	}
+}
