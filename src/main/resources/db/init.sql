12edc306ef2ada513951732a7d77895c4e11e7b8
Hongyuan Wang <710136012@qq.com>
2015-10-29 17:41:35 +0800

    encode file utf8

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -2,10 +2,10 @@ CREATE SCHEMA `fantasy` DEFAULT CHARACTER SET utf8 ;
 
 CREATE TABLE `fantasy`.`article` (
   `id` INT NULL AUTO_INCREMENT,
-  `title` VARCHAR(45) NOT NULL COMMENT '标题',
-  `content` VARCHAR(1024) NULL COMMENT '内容',
+  `title` VARCHAR(45) NOT NULL COMMENT '鏍囬',
+  `content` VARCHAR(1024) NULL COMMENT '鍐呭',
   PRIMARY KEY (`id`))
-COMMENT = '文章';
+COMMENT = '鏂囩珷';
 
 INSERT INTO `fantasy`.`article` (`title`, `content`) VALUES ('hello', 'hello world');
 INSERT INTO `fantasy`.`article` (`title`, `content`) VALUES ('demo', 'demo test');
