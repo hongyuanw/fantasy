dab531fdc752799a36f6ca85b96ea1fe71d7c1f1
Hongyuan Wang <710136012@qq.com>
2015-10-30 15:28:54 +0800

    user mybatis-spring proxy to generate dao implement class

--- src/main/java/com/yuanwhy/fantasy/service/implement/ArticleServiceImpl.java
+++ src/main/java/com/yuanwhy/fantasy/service/implement/ArticleServiceImpl.java
@@ -1,16 +1,12 @@
 package com.yuanwhy.fantasy.service.implement;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.yuanwhy.fantasy.dao.ArticleDao;
 import com.yuanwhy.fantasy.domain.Article;
 import com.yuanwhy.fantasy.service.ArticleService;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
@@ -19,17 +15,13 @@ public class ArticleServiceImpl implements ArticleService {
     private ArticleDao articleDao;
 
     @Override
-    public List<Article> getArticles(int n) {
-        List<Article> list = new ArrayList<Article>();
-        for (int i = 0; i < 3; i++) {
-            list.add(getArticle(i));
-        }
-        return list;
-    }
-
-    @Override
-    @Transactional
     public Article getArticle(int n) {
         return articleDao.getArticle(n);
     }
+
+    @Override
+    public List<Article> getArticles(int n) {
+        return articleDao.getArticles(n);
+    }
+
 }
